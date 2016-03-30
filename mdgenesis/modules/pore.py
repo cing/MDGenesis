e91acfc691b1658964a39e47a74571dae808041e
cing <ing.chris@gmail.com>
2016-03-30 15:57:01 -0400

    HOLE with step, works for averaging now

--- mdgenesis/modules/pore.py
+++ mdgenesis/modules/pore.py
@@ -1,3 +1,4 @@
+import numpy as np
 import mdtraj as md
 import pandas as pd
 from analysis import AllAtOnceAnalysis
@@ -5,14 +6,20 @@ from MDAnalysis.analysis.hole import HOLEtraj
 
 class PoreRadius(AllAtOnceAnalysis):
 
-    def __init__(self, executable="/Users/cing/Code/hole2/exe/hole2"):
+    def __init__(self, step=1, executable="/Users/cing/Code/hole2/exe/hole"):
         """ HOLE is computed for all atoms, with the profile returned
             as per usual.
         """
+        self.step = step
         self.executable = executable
 
     def results(self):
-        H = MDAnalysis.analysis.hole.HOLEtraj(self.u, executable=self.executable,
-                                              cvect=[0,0,1], cpoint=True)
+        H = HOLEtraj(self.u, step=self.step, executable=self.executable,
+                     cvect=[0,0,1], cpoint=True)
         H.run()
-        return pd.DataFrame(H.profiles)
+        # For all frames, make a Series with average rxncoord
+        # and mean across all time points.
+        temp_series=[pd.Series(H.profiles[x]["radius"],
+                               index=np.round(H.profiles[x]["rxncoord"],1)) for x in H.profiles]
+
+        return pd.DataFrame(temp_series).T
